416be3a8076c7c1913204d048f057f7aad6a2436
LucioFex <luciano1D2esteban@gmail.com>
2021-01-06 20:18:01 -0300

    feat: Change in the colors of the calculator and addition of buttons

--- calculatorFunctionality.js
+++ calculatorFunctionality.js
@@ -1,1 +1,16 @@
 // JavaScript document
+
+var terms
+var privacy
+
+function setUp () {
+    terms = document.getElementById("terms")
+    privacy = document.getElementById("privacy")
+
+    for (i=0; i<=1; i++) {
+        
+    }
+}
+
+
+window.addEventListener('load', setUp, false);
