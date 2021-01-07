c27eb865a08e519993b7ca85866e0a3324293055
LucioFex <luciano1D2esteban@gmail.com>
2021-01-07 19:40:16 -0300

    feat, chore: Change of the buttons in the number section and modification of the dates in the LICENSE file.
    feat --> The comma button is now "Euler's number", and the "." is PI.
    In addition of that, now the buttons are in the center of their boxes
    and they with the links to GitHub, Twitter and Email have animations.
    
    chore --> Update of the dates in the MIT LICENSE.

--- calculatorFunctionality.js
+++ calculatorFunctionality.js
@@ -3,14 +3,18 @@
 var terms
 var privacy
 
-// function setUp () {
-//     terms = document.getElementById("terms")
-//     privacy = document.getElementById("privacy")
+function setUp () {
+    terms = document.getElementById("terms");
+    privacy = document.getElementById("privacy");
+    
+    for (element of [terms, privacy]) {
+        element.addEventListener("click", notice, false);
+    }
+}
 
-//     for (i=0; i<=1; i++) {
-        
-//     }
-// }
+function notice(event) {
+    console.log("I WORK FOR", event.target.id) // Check later
+}
 
 
 window.addEventListener('load', setUp, false);
