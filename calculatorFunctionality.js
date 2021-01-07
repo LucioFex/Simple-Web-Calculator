15fb09f7c68f71e35aa8799964075626e14a2e67
LucioFex <luciano1D2esteban@gmail.com>
2021-01-07 11:25:22 -0300

    feat: Aesthetic advance to proceed with JavaScript

--- calculatorFunctionality.js
+++ calculatorFunctionality.js
@@ -3,14 +3,14 @@
 var terms
 var privacy
 
-function setUp () {
-    terms = document.getElementById("terms")
-    privacy = document.getElementById("privacy")
+// function setUp () {
+//     terms = document.getElementById("terms")
+//     privacy = document.getElementById("privacy")
 
-    for (i=0; i<=1; i++) {
+//     for (i=0; i<=1; i++) {
         
-    }
-}
+//     }
+// }
 
 
 window.addEventListener('load', setUp, false);
